cacdd67fa71a806af552730a1be4fccbea3df8e3
Ahmed Hosny <i.ahmedhosny@gmail.com>
2014-08-11 23:14:59 -0400

    html & php

---
+++ 3dprint.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title>
+my 3d printer database
+</title>
+<body>
+
+
+
+<div style="text-align:center;">
+The printer <strong> <?php echo $_POST["printerName"]; ?> </strong> has been added to the database. <br>
+There are now X printers in the database. 
+</div>
+
+
+
+
+
+</body>
+</html>
